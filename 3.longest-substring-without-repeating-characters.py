385f22a5f91a0686a75b40c92521001a3ad49c90
paritoshwankhade <paritosh21w@gmail.com>
2023-05-18 19:33:28 +0530

    New Problem

--- 3.longest-substring-without-repeating-characters.py
+++ 3.longest-substring-without-repeating-characters.py
@@ -7,6 +7,22 @@
 # @lc code=start
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        hash = {}
+        loc_length = 0
+        max_len = 0
+        start = 0
         
+        for i in range(len(s)):
+            if s[i] in hash and hash[s[i]] >= start:
+                start = hash[s[i]] + 1
+            else:
+                loc_length = i - start + 1
+                max_len = max(max_len, loc_length)
+            
+            hash[s[i]] = i
+        
+        return max_len
+
+
 # @lc code=end
 
